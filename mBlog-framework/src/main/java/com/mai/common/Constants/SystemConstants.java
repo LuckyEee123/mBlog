fbd2312947a47bebd33c94d08706805115dd1f07
mai <maisishuai@outlook.com>
2022-12-30 15:58:44 +0800

    更改描述方式，新增分类的常量

--- mBlog-framework/src/main/java/com/mai/common/Constants/SystemConstants.java
+++ mBlog-framework/src/main/java/com/mai/common/Constants/SystemConstants.java
@@ -6,9 +6,15 @@ public class SystemConstants {
      *  文章是草稿
      */
     public static final int ARTICLE_STATUS_DRAFT = 1;
+
     /**
      *  文章是正常分布状态
      */
-    public static final int ARTICLE_STATUS_NORMAL = 0;
+    public static final int ARTICLE_STATUS_PUBLISHED = 0;
+
+    /**
+     *  分类是正常的状态
+     */
+    public static final String CATEGORY_STATUS_NORMAL = "0";
 
 }
