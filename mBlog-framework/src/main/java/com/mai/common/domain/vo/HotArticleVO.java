6bca0b1e2bf49f6cb0bd985705cf75ffb3ff6403
mai <maisishuai@outlook.com>
2022-12-30 13:00:24 +0800

    热门文章VO

---
+++ mBlog-framework/src/main/java/com/mai/common/domain/vo/HotArticleVO.java
@@ -0,0 +1,19 @@
+package com.mai.common.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HotArticleVO {
+
+    private Long id;
+    //标题
+    private String title;
+
+    //访问量
+    private Long viewCount;
+
+}
